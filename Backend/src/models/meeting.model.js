74702a492bb36675df972752a992046241a4c993
Ankit Nautiyal <nautiyalankit65@gmail.com>
2025-02-27 02:32:33 +0530

    setup DB models

---
+++ Backend/src/models/meeting.model.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+const meetingSchema = new Schema(
+    {
+        userid: {type: String},
+        meetingCode: {type: String, required: true},
+        date: {type:Date, default: Date.now, required: true},
+    }
+)
+
+const Meeting = mongoose.model("Meeting", meetingSchema);
+
+export { Meeting };  //used when we need to export many things from this file
+                    // for exporting a single thing we use "export defaut" only
+                    
