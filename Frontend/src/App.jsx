cdbb21d9ae5eb1c08b361c2f065ab975d855c995
Ankit Nautiyal <nautiyalankit65@gmail.com>
2025-03-08 02:42:54 +0530

    created landing page

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import LandingPage from './pages/landing';
+
+function App() {
+
+  return (
+  <div className="App">
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path='/' element={<LandingPage/>} />
+
+      </Routes>
+      
+    </BrowserRouter>
+  </div>
+  )
+}
+
+export default App
--- Frontend/src/pages/landing.jsx
+++ Frontend/src/pages/landing.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import '../App.css';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 export default function LandingPage() {
     return (
-        <div>LandingPage</div>
+        <div className='landingPageContainer'>
+            {/* <BrowserRouter> */}
+                <nav>
+                    <div className='navHeader'>
+                        <h2>Apna Video Call</h2>
+                    </div>
+
+                    <div className='navList'>
+                        <p>Join as Guest</p>
+                        
+                        <p>Register</p>
+
+                        <div role='button'>
+                            <p>Login</p>
+                        </div>
+                    </div>
+                </nav>
+
+                <div className="landingMainContainer">
+                    <div>
+                        <h1>Connect with your loved ones</h1>
+                        <p>Cover a distance by Apna Video Call</p>
+
+                        <div role='button'>
+                            <Link to="/home" >Get Started</Link>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src="/mobile2.png" alt="mobile pic" />
+                    </div>
+                </div>
+            {/* </BrowserRouter> */}
+        
+        </div>
     )
 }
